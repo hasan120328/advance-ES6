a08762b15271103e3158cbf3e1b9b23261fb03bb
hasan120328 <67535336+hasan120328@users.noreply.github.com>
2020-08-07 15:54:09 +0800

    true false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//undifined
+//null
+//''
+//NAN
+//false
+//0
+
+//const name=" "
+//let name;
+//console.log(name);
+let name=0;
+if(name || name==0){
+  console.log('it is true')
+}
+else{
+    console.log('it is false');
+}
